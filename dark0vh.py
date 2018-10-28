475f7cd8a175dfbe5fa54d2682f9d0f6814b2411
dark0vh <37626944+dark0vh@users.noreply.github.com>
2018-10-28 12:52:17 +0000

    Update dark0vh.py

--- dark0vh.py
+++ dark0vh.py
@@ -18,11 +18,11 @@ except: # altrimenti, se fallisce l'importazione
 	
 
 print('''
-print colored(['bold red '], '      DDDDDDDD     AAAAAAA    RRRRRRRR    KK    KK   000000000   VV        VV   HH     HH
-print colored(['bold red '], '      DD     DD   AA     AA   RR    RR    KK  KK     00     00    VV      VV    HH     HH
-print colored(['bold red '], '      DD      DD  AAAAAAAAA   RRRRRRRR    KKKK       00     00     VV    VV     HHHHHHHHH
-print colored(['bold green '],  '   DD     DD   AA     AA   RR  RR      KK  KK     00     00      VV  VV      HH     HH
-print colored(['bold cyan '], '     DDDDDDDD    AA     AA   RR    RR    KK    KK   000000000       VVVV       HH     HH
+print colored(['bold red '], '      DDDDDDDD     AAAAAAA    RRRRRRRR    KK    KK   000000000   VV        VV   HH     HH    ', "\n");
+print colored(['bold red '], '      DD     DD   AA     AA   RR    RR    KK  KK     00     00    VV      VV    HH     HH    ', "\n");
+print colored(['bold red '], '      DD      DD  AAAAAAAAA   RRRRRRRR    KKKK       00     00     VV    VV     HHHHHHHHH    ', "\n");
+print colored(['bold green '],  '   DD     DD   AA     AA   RR  RR      KK  KK     00     00      VV  VV      HH     HH    ', "\n");
+print colored(['bold cyan '], '     DDDDDDDD    AA     AA   RR    RR    KK    KK   000000000       VVVV       HH     HH    ', "\n");
        
              
                                    ANONYMOUS      ALBANIA
